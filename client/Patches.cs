14c5e74599effe0fba504ab5576b944b3bd4b18e
WestCraft15 <24661598+WestCraft15@users.noreply.github.com>
2026-09-04 00:53:58 -0700

    cube port proof of concept

--- client/Patches.cs
+++ client/Patches.cs
@@ -29,6 +29,9 @@ namespace Offipelago
 			return _actorPrefab;
 		}
 
+		private static FPGOverworldMode Overworld { get => FPGOverworldMode.instance; }
+		private static FPGDatabase Database { get => FPGOverworldMode.instance.globalDatabase; }
+
 		/// <summary>
 		/// Gets an unpatched actor by it's <paramref name="eventID"/>. You will rarely need to use this instead of <c>GetActor()</c>.
 		/// </summary>
@@ -87,10 +90,10 @@ namespace Offipelago
 		/// <param name="y">The y coordinate of the new actor.</param>
 		/// <param name="facing">Optionally specify a facing direction for the actor. Default is South</param>
 		/// <returns>The newly created actor. Or null, if an actor with that ID already exists.</returns>
-		private static FPGLogicActor CreateActor(int eventID, int x, int y, GridDirection facing = GridDirection.South)
+		private static FPGLogicActor CreateActor(int eventID, Vector2Int pos, GridDirection facing = GridDirection.South)
 		{
 			if (GetActor(eventID) is not null) return null;
-			var actor = FPGOverworldMode.instance.SpawnNewLogicActor(GetActorPrefab(), new Vector2Int(x, y), facing);
+			var actor = Overworld.SpawnNewLogicActor(GetActorPrefab(), pos, facing);
 			actor.gameObject.hideFlags = HideFlags.None;
 			actor.gameObject.transform.SetParent(GameObject.Find("Actors").transform);
 			actor.GetComponent<FPGLogicActor>().eventID = eventID;
@@ -105,7 +108,29 @@ namespace Offipelago
 		private static void DestroyActor(int eventID)
 		{
 			Object.DestroyImmediate(GetActor(eventID).gameObject);
-			FPGOverworldMode.instance.GetCurrentMapComponent().BuildLogicActorCache();
+			Overworld.GetCurrentMapComponent().BuildLogicActorCache();
+		}
+
+		/// <summary>
+		/// Destroys actors with the specified <paramref name="eventIDs"/>.
+		/// </summary>
+		/// <param name="eventIDs">The ids of the actors to destroy.</param>
+		private static void DestroyActors(params int[] eventIDs)
+		{
+			foreach (var i in eventIDs)
+				Object.DestroyImmediate(GetActor(i).gameObject);
+			Overworld.GetCurrentMapComponent().BuildLogicActorCache();
+		}
+
+		/// <summary>
+		/// Destroys actors with the specified <paramref name="eventIDs"/>.
+		/// </summary>
+		/// <param name="eventIDs">The ids of the actors to destroy.</param>
+		private static void DestroyActors(IEnumerable<int> eventIDs)
+		{
+			foreach (var i in eventIDs)
+				Object.DestroyImmediate(GetActor(i).gameObject);
+			Overworld.GetCurrentMapComponent().BuildLogicActorCache();
 		}
 
 		/// <summary>
@@ -136,18 +161,157 @@ namespace Offipelago
 			};
 		}
 
-		private static FPGSpriteSheet[]? _sheets = null;
+		/// <summary>
+		/// Helper function to create a new line of dialog.
+		/// </summary>
+		/// <param name="text">The text string to display.</param>
+		/// <param name="indent">The indent of the command.</param>
+		/// <returns>A new FPGCmdShowTextUntranslated that contains your text string.</returns>
+		private static FPGCmdShowTextUntranslated CreateText(string text, int indent)
+		{
+			return new FPGCmdShowTextUntranslated()
+			{
+				text = text,
+				indent = indent
+			};
+		}
 
+		// The possible colors for a cube port.
+		enum CubePortColor
+		{
+			Yellow,
+		}
+
+		/// <summary>
+		/// Helper function to create a new cube port.
+		/// </summary>
+		/// <param name="actorID">The id of the new actor.</param>
+		/// <param name="pos">The position of the cube port.</param>
+		/// <param name="name">The name of the cube port.</param>
+		/// <param name="color">The color of the cube, typically based on the zone.</param>
+		/// <param name="activationCommands">A list of FPGCommands to run on successful activation.</param>
+		/// <returns>The new cube port actor.</returns>
+		private static FPGLogicActor CreateCubePort(int actorID, Vector2Int pos, bool isPuzzle, string name, CubePortColor color, IEnumerable<FPGCommand> activationCommands)
+		{
+			var cube_port = CreateActor(actorID, pos);
+			cube_port.states[0].spriteSheet = GetSpriteSheet("cubeDown_yellow");
+			cube_port.states[0].commands = [
+				new FPGCmdShowTextPortrait() { portrait = GetSprite("le batteur_0") },
+				CreateText("It's a cube port."),
+				new FPGCmdIf(),
+					new FPGCmdShowChoices() { indent = 1, choices = ["Place cube", "Do nothing"] },
+					new FPGCmdIfChoice() { indent = 1, choice = "Place cube" },
+						new FPGCmdShowAnimatedEffect() { indent = 2, effectID = 227, targetActor = new LogicActorReference(actorID) },
+						new FPGCmdShowTextPortrait() { indent = 2 },
+						CreateText("The cube slots into the hole.", 2),
+					new FPGCmdEndIf() { indent = 1 },
+				new FPGCmdElse(),
+					CreateText($"It's labeled “{name}”", 1),
+				new FPGCmdEndIf(),
+			];
+			cube_port.states[0].commands.InsertRange(8, activationCommands.Select(c => { c.indent += 2; return c; }));
+			cube_port.Init();
+			return cube_port;
+		}
+
+
+		/// <summary>
+		/// Helper function to create a new cube port.
+		/// </summary>
+		/// <param name="actorID">The id of the new actor.</param>
+		/// <param name="pos">The position of the cube port.</param>
+		/// <param name="name">The name of the cube port.</param>
+		/// <param name="color">The color of the cube, typically based on the zone.</param>
+		/// <param name="activationCommand">An FPGCommand to run on successful activation.</param>
+		/// <returns>The new cube port actor.</returns>
+		private static FPGLogicActor CreateCubePort(int actorID, Vector2Int pos, bool isPuzzle, string name, CubePortColor color, FPGCommand activationCommand)
+		{
+			return CreateCubePort(actorID, pos, isPuzzle, name, color, [activationCommand]);
+		}
+
+		private static Dictionary<string, FPGSpriteSheet> _sheets = [];
+
+		/// <summary>
+		/// Gets the sprite sheet with the specified name.
+		/// </summary>
+		/// <param name="name">The name of the sprite sheet to find.</param>
+		/// <returns>The sprite sheet, or null if none were found.</returns>
 		private static FPGSpriteSheet GetSpriteSheet(string name)
 		{
-			_sheets = Resources.FindObjectsOfTypeAll<FPGSpriteSheet>();
-			return _sheets.FirstOrDefault(o => o.name == name);
+			// Only refresh cache if a missing sheet is requested
+			if (!_sheets.ContainsKey(name))
+			{
+				_sheets = Resources.FindObjectsOfTypeAll<FPGSpriteSheet>().ToDictionary(s => s.name);
+			}
+			return _sheets.GetValueSafe(name);
+		}
+
+		private static Dictionary<string, Sprite> _sprites = [];
+
+		/// <summary>
+		/// Gets the sprite with the specified name.
+		/// Due to name collisions, this rarely may not return the expected sprite.
+		/// Collisions are mitigated first by adding textureRect.x and textureRect.y,
+		/// then by adding texture.name instead. If there's still a collision, the sprite is simply excluded.
+		/// Please message West if you're having issues.
+		/// </summary>
+		/// <param name="name">The name of the sprite to find.</param>
+		/// <returns>The sprite, or null if none were found.</returns>
+		private static Sprite GetSprite(string name)
+		{
+			// Only refresh cache if a missing sheet is requested
+			if (!_sprites.ContainsKey(name))
+			{
+				var sprites = Resources.FindObjectsOfTypeAll<Sprite>();
+				foreach (var sprite in sprites) {
+					if (!_sprites.TryAdd(sprite.name, sprite))
+						if (!_sprites.TryAdd($"{sprite.name}-{sprite.textureRect.x}-{sprite.textureRect.y}", sprite))
+							_sprites.TryAdd($"{sprite.name}-{sprite.texture.name}", sprite);
+				}
+			}
+			return _sprites.GetValueSafe(name);
 		}
 
 		public static void Post_003()
 		{
 			PatchChest(10, 1000000);
 			PatchChest(11, 1000001);
+		}
+
+		public static void Post_004()
+		{
+			var pablo = GetActor(14);
+			pablo.states[0].commands = [
+				pablo.states[0].commands[0],
+				pablo.states[0].commands[1],
+				CreateText("In this place, puzzles remain dormant until one acquires the necessary “Puzzle Cube.”"),
+				CreateText("Once it is found, I implore you to locate the “Cube Port” within the room and place the aforementioned object therein."),
+				CreateText("You will then find the puzzle to be activated, and thusly solvable."),
+				CreateText("I shall grant you the Puzzle Cube for this room. I believe you will find it quite useful for this upcoming obstacle."),
+				new FPGCmdShowTextPortrait(),
+				CreateText("A Puzzle Cube (Zone 0 Top Floor) has been found."),
+			];
+
+			CreateCubePort(15, new(8, 8), true, "Zone 0 Top Floor", CubePortColor.Yellow, []);
+		}
+
+		public static void Post_006()
+		{
+			var pablo = GetActor(22);
+			pablo.states[0].commands = [
+				pablo.states[0].commands[0],
+				pablo.states[0].commands[1],
+				CreateText("Ah, a similar concept. How delightful."),
+				CreateText("This room requires an “Access Cube” to proceed."),
+				CreateText("It functions in much the same way as a Puzzle Cube, but will simply remove the obstruction without the need to activate any additional floating geometric objects."),
+				CreateText("Here, I shall once again grant you the required artifact posthaste."),
+				new FPGCmdShowTextPortrait(),
+				CreateText("A Puzzle Cube (Zone 0 Middle Floor) has been found."),
+			];
+
+			CreateCubePort(23, new(11, 8), false, "Zone 0 Middle Floor", CubePortColor.Yellow, GetActor(4).states[1].commands.GetRange(9, 5));
+
+			DestroyActors(1, 2, 3, 4, 8, 9, 12, 13, 14, 15, 16, 17, 18, 19);
 		}
 
 		public static void Post_015()
@@ -297,12 +461,13 @@ namespace Offipelago
 	// A version of FPGCmdShowText that doesn't try to get a translated string.
 	internal class FPGCmdShowTextUntranslated : FPGCmdShowText
 	{
-		public FPGCmdShowTextUntranslated(FPGCmdShowText? old)
+		public FPGCmdShowTextUntranslated(FPGCmdShowText? old = null)
 		{
 			if (old != null)
 			{
 				text = old.text;
 				autoOff = old.autoOff;
+				indent = old.indent;
 			}
 		}
 
